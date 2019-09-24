c998c18335565fad40b750ca05fda4de21f61885
TIAN ZHONGBAO <JP26142@w022581902977m.local>
2019-09-24 12:24:42 +0900

    change hello-world

--- hello-world/hello-world.go
+++ hello-world/hello-world.go
@@ -1,3 +1,11 @@
+// 我们的第一个程序将打印传说中的 “hello world“ 消息，右边是完整的程序代码。
+//
+// 要运行这个程序，将代码放到 hello-world.go 中 然后执行 go run 。
+// 有时候我们想将程序编译成二进制文件， 可以通过 go build 来达到目的。
+// 然后我们可以直接运行这个二进制文件。
+// 现在我们可以运行并编译基本的 Go 程序，让我们开始学习更多 关于这门语言的知识吧。
+//
+// https://books.mlog.club/gobyexample/hello-world.html
 package main
 
 import "fmt"
