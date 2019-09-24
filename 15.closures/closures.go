afd8194b8d00c3e3a71c4d1a7ee8616c619bb2fe
TIAN ZHONGBAO <JP26142@w022581902977m.local>
2019-09-24 17:34:20 +0900

    add closures

---
+++ 15.closures/closures.go
@@ -0,0 +1,42 @@
+// Go 支持[匿名函数](https://zh.wikipedia.org/wiki/%E5%8C%BF%E5%90%8D%E5%87%BD%E6%95%B0)，
+// 并能用其构造[闭包](https://zh.wikipedia.org/wiki/%E9%97%AD%E5%8C%85_(%E8%AE%A1%E7%AE%97%E6%9C%BA%E7%A7%91%E5%AD%A6))。
+// 匿名函数在你想定义一个不需要命名的内联函数时是很实用的。
+// 我们马上要学习关于函数的最后一个特性：递归。
+//
+// https://books.mlog.club/gobyexample/closures.html
+package main
+
+import "fmt"
+
+// 这个 intSeq 函数返回另一个在 intSeq 函数体内定义的匿名函数。这个返回的函数使用闭包的方式隐藏变量 i。
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+
+	// 我们调用 intSeq 函数，将返回值（一个函数）赋给 nextInt。
+	// 这个函数的值包含了自己的值 i，这样在每次调用 nextInt 时都会更新 i 的值。
+	nextInt := intSeq()
+
+	// 通过多次调用 nextInt 来看看闭包的效果。
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	// 为了确认这个状态对于这个特定的函数是唯一的，我们重新创建并测试一下。
+	newInts := intSeq()
+	fmt.Println(newInts())
+}
+
+// Output
+/*
+	1
+	2
+	3
+	1
+*/
