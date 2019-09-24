b14bd8e814b5aa8c02024faf3be1fb69fc29d4d8
TIAN ZHONGBAO <JP26142@w022581902977m.local>
2019-09-24 12:26:55 +0900

    change hello-world and values

--- values/values.go
+++ values/values.go
@@ -1,3 +1,10 @@
+// Go 拥有多种值类型，包括字符串，整型，浮点型，布尔 型等。下面是一些基本的例子。
+//
+// 字符串可以通过 + 连接。
+// 整数和浮点数
+// 布尔型，以及常见的布尔操作。
+//
+// https://books.mlog.club/gobyexample/values.html
 package main
 
 import "fmt"
